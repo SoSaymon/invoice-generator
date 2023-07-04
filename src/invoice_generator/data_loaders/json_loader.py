e1753de48b19ca6c6d357d167e7d59c83b11e98c
SoSaymon <szymon.chirowski@protonmail.com>
2023-07-04 14:51:07 +0200

    fix(json_loader): fix not raising errors in load_json function

--- src/invoice_generator/data_loaders/json_loader.py
+++ src/invoice_generator/data_loaders/json_loader.py
@@ -8,6 +8,12 @@ def load_json(file_path: str) -> list[dict]:
     :return: list[dict] - list of dictionaries with data
     """
 
-    with open(file_path, 'r') as json_file:
-        data: list[dict] = json.load(json_file)
+    try:
+        with open(file_path, 'r') as json_file:
+            data: list[dict] = json.load(json_file)
+    except FileNotFoundError:
+        raise FileNotFoundError(f'File {file_path} not found')
+    except json.JSONDecodeError:
+        raise json.JSONDecodeError(f'File {file_path} is empty', '', 0)
+
     return data
